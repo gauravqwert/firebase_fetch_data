74da9301dae9933e5e7d839f7b12fd8251487564
unknown <patelgaurav282000@gmail.com>
2024-07-19 18:23:03 +0530

    Fetch data from the firebase in React Native

---
+++ App.js
@@ -0,0 +1,16 @@
+import { StyleSheet, View } from 'react-native';
+import Fetch from './src/Fetch';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+         <Fetch/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
